00223779825ceccc80f27ca31410979d0822e2fd
Evan Winter <ewinter1996@gmail.com>
2017-05-04 03:16:59 -0500

    fixed everything

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-<<<<<<< HEAD
 	$('.post').click(function() {
 		$(this).children('.post-body').toggleClass('hide');
 	});
-=======
-	// ...
->>>>>>> b5e88ceae1a5719958c09e263f6852e6d05795ff
 });
