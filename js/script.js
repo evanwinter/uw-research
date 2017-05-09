6c3f6fdbcfdbd0527fbcdc44d7d4758f29acdd98
Evan Winter <ewinter1996@gmail.com>
2017-05-08 23:41:27 -0500

    .

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
 		var thisScrollTop = $(this).offset().top
 
-		$("html, body").animate({ scrollTop: thisScrollTop }, "slow");
+		$("html, body").animate({ scrollTop: thisScrollTop-5 }, "slow");
 
 		// $(this).children('.post-info').children('.click-to-expand-group').toggle();
 		$(this).children('.post-body').slideToggle('fast');
